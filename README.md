4c2326418d0e0a1e682a9984256f2abcbe5a9b3a
Richard S. Leung <r4ds@lenyip.com>
2025-01-13 03:25:44 +0900

    Lint

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # dns-spa
-Single Page Application built with Svelte for DoH requests.  
-This is a playground project built for my own convenience to have a site that can summarize domain's DNS records quick.  
 
-# Deploying  
+Single Page Application built with Svelte for DoH requests.  
+This is a playground project built for my own convenience to have a site that can summarize domain's DNS records quick.
+
+# Deploying
 
 https://github.com/metonym/sveltekit-gh-pages  
-https://kit.svelte.dev/docs/adapter-static   
+https://kit.svelte.dev/docs/adapter-static
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,80 +1,116 @@
 <script lang="ts">
-import Keydown from "svelte-keydown";
-let domain = '';
-let count = 0;
-export const apiMap = new Map();
+	import Keydown from 'svelte-keydown';
+	let domain = '';
+	let count = 0;
+	export const apiMap = new Map();
 
-import { onMount } from 'svelte';
-import { page } from '$app/stores'
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 
-onMount(() => {
-    const getquery = $page.url.searchParams.get('query')
-    if ( getquery ) {
-        domain = getquery;
-        // Not to automatically trigger the request as of now
-        // document.getElementById("sum")?.click();
-    }
-})
+	onMount(() => {
+		const getquery = $page.url.searchParams.get('query');
+		if (getquery) {
+			domain = getquery;
+			// Not to automatically trigger the request as of now
+			// document.getElementById("sum")?.click();
+		}
+	});
 
-// let baseurl = "https://cloudflare-dns.com/dns-query?"
-let baseurl = "https://dns.google/resolve?"
+	// let baseurl = "https://cloudflare-dns.com/dns-query?"
+	let baseurl = 'https://dns.google/resolve?';
 
-let dnstype = ["SOA", "A", "AAAA", "TXT", "MX", "NS", "CAA"]
-const emailcheck: { [x: string]: string; } = {
-    '_dmarc': "TXT",
-    '_spf': "TXT",
-    'mta-sts': "A",
-    '_mta-sts': "TXT",
-    '_smtp._tls': "TXT",
-    'google._domainkey': "TXT"
-};
+	let dnstype = ['SOA', 'A', 'AAAA', 'TXT', 'MX', 'NS', 'CAA'];
+	const emailcheck: { [x: string]: string } = {
+		_dmarc: 'TXT',
+		_spf: 'TXT',
+		'mta-sts': 'A',
+		'_mta-sts': 'TXT',
+		'_smtp._tls': 'TXT',
+		'google._domainkey': 'TXT'
+	};
 
-// https://svelte.dev/repl/cb31be94ea444b41a11d1320d16ba6dc?version=3.32.3
-async function requestDoH () {
-    updateURL();
-    apiMap.clear()
+	// https://svelte.dev/repl/cb31be94ea444b41a11d1320d16ba6dc?version=3.32.3
+	async function requestDoH() {
+		updateURL();
+		apiMap.clear();
 
-    // dnstype check
-    for (const type of dnstype) {
-        await fetch(baseurl + "name=" + domain + "&type=" + type, {headers: {"accept": "application/dns-json"}})
-        .then(response => response.json())
-        .then(data => {
-            if( data.Answer ) {
-                apiMap.set(type, data.Answer)
-                console.log("setting", type)
-            }
-        }).catch(error => {
-            console.log(error);
-            return [];
-        });
-    }
+		// dnstype check
+		for (const type of dnstype) {
+			await fetch(baseurl + 'name=' + domain + '&type=' + type, {
+				headers: { accept: 'application/dns-json' }
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					if (data.Answer) {
+						apiMap.set(type, data.Answer);
+						console.log('setting', type);
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+					return [];
+				});
+		}
 
-    // special email records check on subdomains
-    for (let key in emailcheck) {
-        await fetch(baseurl + "name=" + key + "." + domain + "&type=" + emailcheck[key], {headers: {"accept": "application/dns-json"}})
-        .then(response => response.json())
-        .then(data => {
-            if( data.Answer ) {
-                apiMap.set(key, data.Answer)
-                console.log("setting", key)
-            }
-        }).catch(error => {
-            console.log(error);
-            return [];
-        });
-    }
+		// special email records check on subdomains
+		for (let key in emailcheck) {
+			await fetch(baseurl + 'name=' + key + '.' + domain + '&type=' + emailcheck[key], {
+				headers: { accept: 'application/dns-json' }
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					if (data.Answer) {
+						apiMap.set(key, data.Answer);
+						console.log('setting', key);
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+					return [];
+				});
+		}
 
-    count++;
-    console.log("returning.");
-}
+		count++;
+		console.log('returning.');
+	}
 
-function updateURL() {
-    const url = new URL(window.location);
-    url.searchParams.set('query', domain);
-    window.history.pushState({}, '', url);
-}
-
+	function updateURL() {
+		const url = new URL(window.location);
+		url.searchParams.set('query', domain);
+		window.history.pushState({}, '', url);
+	}
 </script>
+
+<p>
+	This site is built with Svelte as a playground project, which sends request to a DoH server. <a
+		href="https://github.com/richardevs/dns-spa"
+		target="_blank">Source</a
+	>
+</p>
+<p>It queries the common record types for the domain. Format: TTL, value</p>
+<h1>
+	<input bind:value={domain} placeholder="enter desired domain" />
+	<button id="sum" on:click={requestDoH}>Summarize</button>
+</h1>
+
+<Keydown
+	on:Enter={() => {
+		document.getElementById('sum')?.click();
+	}}
+/>
+
+<!-- https://svelte.dev/repl/db8ac032184b455bbeed903ba042937c?version=4.2.17 -->
+{#key count}
+	<!-- <li>Debug: {count}</li> -->
+	<!-- https://github.com/sveltejs/svelte/issues/5021 -->
+	{#each [...apiMap] as [key, value]}
+		<!-- <li>{key} => {Object.getOwnPropertyNames(value)}</li> -->
+		<h1>{key}</h1>
+		{#each value as item}
+			<li>{item.TTL}, {item.data}</li>
+		{/each}
+	{/each}
+{/key}
 
 <!-- <style>
 table, th, td {
@@ -84,32 +120,7 @@ table, th, td {
 </style> -->
 
 <style>
-    li {
-        word-wrap: break-word;
-    }
+	li {
+		word-wrap: break-word;
+	}
 </style>
-
-<p>This site is built with Svelte as a playground project, which sends request to a DoH server. <a href="https://github.com/richardevs/dns-spa" target="_blank">Source</a></p>
-<p>It queries the common record types for the domain. Format: TTL, value</p>
-<h1>
-    <input bind:value={domain} placeholder="enter desired domain" /> <button id="sum" on:click={requestDoH}>Summarize</button>
-</h1>
-
-<Keydown
-  on:Enter={() => {
-    document.getElementById("sum")?.click();
-  }}
-/>
-
-<!-- https://svelte.dev/repl/db8ac032184b455bbeed903ba042937c?version=4.2.17 -->
-{#key count}
-    <!-- <li>Debug: {count}</li> -->
-    <!-- https://github.com/sveltejs/svelte/issues/5021 -->
-    {#each [...apiMap] as [key, value]}
-        <!-- <li>{key} => {Object.getOwnPropertyNames(value)}</li> -->
-        <h1>{key}</h1>
-        {#each value as item}
-        <li>{item.TTL}, {item.data}</li>
-        {/each}
-    {/each}
-{/key}
